d1f1a594a789802630a8a830d6d9c8db82dcb2d7
Randy McMillan <randy.lee.mcmillan@gmail.com>
2018-03-01 01:07:41 -0500

    modified:   MobileMiner/AppDelegate.m

--- MobileMiner/AppDelegate.m
+++ MobileMiner/AppDelegate.m
@@ -29,7 +29,9 @@ static BOOL hasSavedDev=NO;
 
 //REF:https://democats.org
 //#define DEFAULTURL "stratum+tcp://bcn.pool.minergate.com:45620"
-#define DEFAULTURL "stratum+tcp://bcnpool.space:3333"
+#define DEFAULTURL "stratum+tcp://bcnpool.space:3333"//diff 100
+//#define DEFAULTURL "stratum+tcp://bcnpool.space:5555"//diff 2000
+//#define DEFAULTURL "stratum+tcp://bcnpool.space:7777"//diff 10000
 //#define DEFAULTURL "stratum+tcp://pool.democats.org:45500"
 //#define DEFAULTUSER "randy.lee.mcmillan@gmail.com"
 #define DEFAULTUSER "269J6NCx5gxhG6C6Eezgw26NmyzyzeNrDB1v26dxuaA1LMKMBnehJTcF8d3vy9YnjxavhyxH3XtiQ2iE8aXhTT8sVd4CMUy"
